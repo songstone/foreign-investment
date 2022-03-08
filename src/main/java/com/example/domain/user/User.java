fc10b7a100f6094b76e9381f775a3e2c2317f04c
Song Seung-hyun <thdtmdgus5505@gmail.com>
2022-03-09 01:28:10 +0900

    feat: create User

---
+++ src/main/java/com/example/domain/user/User.java
@@ -0,0 +1,28 @@
+package com.example.domain.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String email;
+
+    private LocalDate birth;
+
+}
