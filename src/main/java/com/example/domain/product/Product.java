907fb50b08950d1817b89610b59bd88ea358ac7e
Song Seung-hyun <thdtmdgus5505@gmail.com>
2022-03-09 01:28:58 +0900

    feat: create Product

---
+++ src/main/java/com/example/domain/product/Product.java
@@ -0,0 +1,39 @@
+package com.example.domain.product;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    private BigDecimal totalInvestingAmount;
+
+    private BigDecimal investedAmount;
+
+    private Long investedCount;
+
+    private String country;
+
+    private LocalDate startAt;
+
+    private LocalDate finishAt;
+}
