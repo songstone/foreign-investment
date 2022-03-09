6f2c2785ff657306191f681a25787095fc94c724
Song Seung-hyun <thdtmdgus5505@gmail.com>
2022-03-09 13:50:38 +0900

    feat: create constant
    - InvestmentStatus
    - TransferType

---
+++ src/main/java/com/example/constant/TransferType.java
@@ -0,0 +1,14 @@
+package com.example.constant;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum TransferType {
+    WITHDRAW("출금"),
+    DEPOSIT("입금")
+    ;
+
+    private final String descrption;
+}
