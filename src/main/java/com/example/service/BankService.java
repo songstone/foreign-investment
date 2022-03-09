32083b3ef2f734d33aeea29667c17618cee955db
Song Seung-hyun <thdtmdgus5505@gmail.com>
2022-03-09 14:04:17 +0900

    feat: implement registerAccount, getAccount

---
+++ src/main/java/com/example/service/BankService.java
@@ -0,0 +1,32 @@
+package com.example.service;
+
+import com.example.domain.account.Account;
+import com.example.domain.account.AccountRepository;
+import com.example.domain.user.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+
+@RequiredArgsConstructor
+@Service
+public class BankService {
+
+    private final AccountRepository accountRepository;
+
+    public Long registerAccount(User user, String password) {
+        Account account = Account.builder()
+                .user(user)
+                .password(password)
+                .balance(new BigDecimal(0))
+                .build();
+        return accountRepository.save(account).getAccountNum();
+    }
+
+    public Account getAccount(Long accountNum, String password) {
+        Account account = accountRepository.findByAccountNumAndPassword(accountNum, password).orElseThrow(
+                () -> new IllegalArgumentException("NOT_INVALID_ACCOUNT")
+        );
+        return account;
+    }
+}
