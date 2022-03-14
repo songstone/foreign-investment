77e282402d8e69a2fe0ae17deb304b39ecc49bee
Song Seung-hyun <thdtmdgus5505@gmail.com>
2022-03-14 23:04:01 +0900

    feat: divide case

--- src/main/java/com/example/utils/ExchangeRateCalculator.java
+++ src/main/java/com/example/utils/ExchangeRateCalculator.java
@@ -1,5 +1,6 @@
 package com.example.utils;
 
+import com.example.constant.UnitCode;
 import com.example.dto.ExchangeRateData;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -14,11 +15,21 @@ public class ExchangeRateCalculator {
     private final String AUTH_KEY = "SKOpPq41rNSDIy4gZdb1wUo9xC2vDiBI";
     private String url = "https://www.koreaexim.go.kr/site/program/financial/exchangeJSON?authkey=" + AUTH_KEY + "&data=AP01";
 
-    public ExchangeRateData exchangeUnit(String unitCode) {
+    public String exchangeUnitForSend(UnitCode unitCode) {
         ExchangeRateData[] result = restTemplate.getForObject(url, ExchangeRateData[].class);
         for (ExchangeRateData unit : result) {
-            if (unit.getCur_unit().substring(0,3).equals(unitCode)) {
-                return unit;
+            if (unit.getCur_unit().substring(0,3).equals(unitCode.name())) {
+                return unit.getTts();
+            }
+        }
+        throw new IllegalArgumentException("COUNTRY_NOT_FOUND");
+    }
+
+    public String exchangeUnitForGet(UnitCode unitCode) {
+        ExchangeRateData[] result = restTemplate.getForObject(url, ExchangeRateData[].class);
+        for (ExchangeRateData unit : result) {
+            if (unit.getCur_unit().substring(0,3).equals(unitCode.name())) {
+                return unit.getTtb();
             }
         }
         throw new IllegalArgumentException("COUNTRY_NOT_FOUND");
