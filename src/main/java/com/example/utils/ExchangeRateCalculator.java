339cd8daa3175d1d37341f83965e4b5ddd249a1b
Song Seung-hyun <thdtmdgus5505@gmail.com>
2022-03-15 18:11:37 +0900

    fix: fix get UnitCode form

--- src/main/java/com/example/utils/ExchangeRateCalculator.java
+++ src/main/java/com/example/utils/ExchangeRateCalculator.java
@@ -6,6 +6,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
+import java.math.BigDecimal;
+
 @RequiredArgsConstructor
 @Component
 public class ExchangeRateCalculator {
@@ -15,21 +17,21 @@ public class ExchangeRateCalculator {
     private final String AUTH_KEY = "SKOpPq41rNSDIy4gZdb1wUo9xC2vDiBI";
     private String url = "https://www.koreaexim.go.kr/site/program/financial/exchangeJSON?authkey=" + AUTH_KEY + "&data=AP01";
 
-    public String exchangeUnitForSend(UnitCode unitCode) {
+    public BigDecimal exchangeUnitForSend(UnitCode unitCode) {
         ExchangeRateData[] result = restTemplate.getForObject(url, ExchangeRateData[].class);
         for (ExchangeRateData unit : result) {
-            if (unit.getCur_unit().substring(0,3).equals(unitCode.name())) {
-                return unit.getTts();
+            if (unit.getCur_unit().substring(0, 3).equals(unitCode.name())) {
+                return new BigDecimal(unit.getTts().replaceAll(",", ""));
             }
         }
         throw new IllegalArgumentException("COUNTRY_NOT_FOUND");
     }
 
-    public String exchangeUnitForGet(UnitCode unitCode) {
+    public BigDecimal exchangeUnitForGet(UnitCode unitCode) {
         ExchangeRateData[] result = restTemplate.getForObject(url, ExchangeRateData[].class);
         for (ExchangeRateData unit : result) {
-            if (unit.getCur_unit().substring(0,3).equals(unitCode.name())) {
-                return unit.getTtb();
+            if (unit.getCur_unit().substring(0, 3).equals(unitCode.name())) {
+                return new BigDecimal(Long.parseLong(unit.getTtb().replaceAll(",", "")));
             }
         }
         throw new IllegalArgumentException("COUNTRY_NOT_FOUND");
